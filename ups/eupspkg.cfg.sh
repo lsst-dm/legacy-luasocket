9e351cd2f04aeefb0db311ca80894afabdfb2535
Mario Juric <mjuric@lsst.org>
2014-12-14 01:02:38 -0800

    OS X compatibility: use different config file for Darwin builds

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,6 +2,15 @@
 
 config()
 {
+    case $(uname) in
+    Linux)
+        cp ups/config.Linux config ;;
+    Darwin)
+        cp ups/config.Darwin config ;;
+    *)
+        die "Unsupported OS $(uname). Aborting." ;;
+    esac
+
     [[ $(uname) = Darwin ]] && SED_INPLACE="sed -i '.prev'" || SED_INPLACE="sed -i"
 
     $SED_INPLACE "s,INSTALL_TOP_SHARE=/usr/local/share/lua/5.1,LUAINC=-I${LUA_DIR}/include"\\$'\n'"INSTALL_TOP_SHARE=${LUA_DIR}/share/lua/5.1," config &&
