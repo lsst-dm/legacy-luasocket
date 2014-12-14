e4219ee64bad87dd09ffafb1db2c1673cab284bb
Mario Juric <mjuric@lsst.org>
2014-12-14 01:00:38 -0800

    OS X compatibility: Darwin sed -i requires an argument

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,6 +2,8 @@
 
 config()
 {
-    sed -i'.orig' "s,INSTALL_TOP_SHARE=/usr/local/share/lua/5.1,LUAINC=-I${LUA_DIR}/include\nINSTALL_TOP_SHARE=${LUA_DIR}/share/lua/5.1," config &&
-    sed -i'.orig' "s,INSTALL_TOP_LIB=/usr/local/lib/lua/5.1,INSTALL_TOP_LIB=${LUA_DIR}/lib/lua/5.1," config 
+    [[ $(uname) = Darwin ]] && SED_INPLACE="sed -i '.prev'" || SED_INPLACE="sed -i"
+
+    $SED_INPLACE "s,INSTALL_TOP_SHARE=/usr/local/share/lua/5.1,LUAINC=-I${LUA_DIR}/include\nINSTALL_TOP_SHARE=${LUA_DIR}/share/lua/5.1," config &&
+    $SED_INPLACE "s,INSTALL_TOP_LIB=/usr/local/lib/lua/5.1,INSTALL_TOP_LIB=${LUA_DIR}/lib/lua/5.1," config 
 }
